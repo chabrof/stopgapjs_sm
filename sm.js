ec857ba2d9b7bb1230dcfc52e28fe33c6dc349d1
François Chabrou <francois.chabrou.prestataire@sfr.com>
2016-03-08 18:33:53 +0100

    next

--- sm.js
+++ sm.js
@@ -11,10 +11,11 @@ define([], function() {
     // stores states by ref in a hash with .id as key
     config.states.forEach(function(state) {
         self._statesHash[state.id] = state
+				//self.
       })
 
     // root state
-    self._rootStateId = config.rootStateId
+    self._curStateId = config.rootStateId
 
     // store transition in a hash with .id as key
     // store transitions which move from states, in hash with state.id as key
@@ -27,18 +28,59 @@ define([], function() {
       })
   }
 
-  ClassP.getRootState = function() {
-    return this._transitionHash[this._rootStateId];
+  ClassP.getCurState = function() {
+    return this._statesHash[this._curStateId]
   }
 
-  ClassP._getAvailableTransIds = function(state) {
+  var State_getAvailableTrans = function() {
+		return this._sgjsm._statesId2Trans[this._curStateId]
   }
 
-  ClassP._injectMethods = function(state) {
+  ClassP._injectMethodsIntoState = function(state) {
+		state._sgjsm = this;
+		state.getAvailableTrans = State_getAvailableTrans
+  }
 
+	var Transition_execute = function() {
+		this._curStateId = this.dstStateId
+	}
+
+	ClassP._injectMethodsIntoTrans = function(transition) {
+		transition._sgjsm = this;
+		transition.execute = Transition_execute;
   }
 
 
+	ClassP.addEventListener = function(type, cbk) {
+		if (typeof type === "string" && typeof cbk === "function" && type && cbk) {
+			this._listenerId++
+
+			var listener = {
+				type 	: type,
+				cbk 	: cbk
+			}
+
+			if (! this._listenersHash[type]) {
+				this._listenersHash[type] = {}
+			}
+			this._listenersHash[type][this.listenerId] = listener
+		} else {
+			throw "not valid EventListener adding"
+		}
+	}
+
+	/*event = new CustomEvent(attribName, { "detail" : { "exec" : true, "after" : true }})
+                  self.dispatchEvent(event)
+								*/
+	ClassP.dispatchEvent = function(type, obj) {
+
+		if (this._listenersHash[type]) {
+			for (var i in this._listenersHash[eventName]) {
+				console.assert(i.cbk, 'cbk is not define for listener')
+				i.cbk(type, obj);
+			}
+		}
+	}
 
   return StopGapJs_sm
 })
