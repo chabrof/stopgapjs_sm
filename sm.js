fc1cb87cb27f33fd2e398c3e7ea404e9ec9a4dbd
François Chabrou <francois.chabrou.prestataire@sfr.com>
2016-03-31 16:15:19 +0200

    Refactor TU

--- sm.js
+++ sm.js
@@ -8,7 +8,6 @@ define([], function() {
 
   ClassP.init = function(config) {
     var self = this
-    self._listenerIdx = 0
     self._listenersByType = {}
     self._statesById = {}
     self._transitionsByStateId = {}
@@ -56,6 +55,10 @@ define([], function() {
   }
 
   var Transition_execute = function() {
+		// pre
+		console.assert(this._sgjsm.getCurState().id === this.srcStateId,
+			'You can not execute a transition whom source state is not the cur state of the state machine')
+
     this._sgjsm._curStateId = this.dstStateId
     var event = (typeof CustomEvent !== 'undefined' ? new CustomEvent('transit', { "detail" : { "transtion" : this }}) :
     { "type" : "transit", "detail" : { "transition" : this }})
@@ -70,32 +73,41 @@ define([], function() {
   }
 
   ClassP.addEventListener = function(type, cbk) {
-    // pre
+		// pre
     console.assert(
       typeof type === "string" && typeof cbk === "function" && type && cbk,
       "Arg 'Type' (string) of event must be given, as well as cbk (function)" )
 
-    this._listenerIdx++
 
     var listener = {
       type  : type,
       cbk   : cbk
     }
-    this._listeners[this._listenerIdx] = listener
+    this._listeners.push(listener)
     if (! this._listenersByType[type]) {
       this._listenersByType[type] = []
     }
     this._listenersByType[type].push(listener)
     listener.arrayIdx = this._listenersByType[type].length - 1
+		return this._listeners.length - 1
   }
 
   ClassP.removeEventListener = function(listenerIdx) {
-    var listener = this._listeners[listenerIdx] = undefined
-    var type = listener.type
+		// pre
+		console.assert(	listenerIdx !== undefined && listenerIdx !== null && listenerIdx >= 0,
+										"listenerIdx must be a not null integer")
+
+    var listener = this._listeners[listenerIdx]
+
+		console.assert(listener, "listener (" + listenerIdx + ") must be a not null, maybe you have removed the listener twice", this._listeners)
+
+		var type = listener.type
+		this._listenersByType[type].splice(listener.arrayIdx, 1)
     for (var idx = listener.arrayIdx + 1; idx < this._listenersByType[type].length; idx++) {
       console.assert(this._listenersByType[type][idx].arrayIdx >= 0, 'arrayIdx for a listener event must be an integer');
       --(this._listenersByType[type][idx].arrayIdx)
     }
+		this._listeners[listenerIdx] = undefined
   }
 
   /*event = new CustomEvent(type, { "detail" : { "exec" : true, "after" : true }})
--- test/main.js
+++ test/main.js
@@ -1,91 +1,118 @@
 var assert = require('assert')
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
-define(['require'], // here we get the requirejs object in order to set the cur path to the path of this top level file
-  function (requirejs) {
+
+define(['require', '../sm.js'], // here we get the requirejs object in order to set the cur path to the path of this top level file
+  function (requirejs, StopGapJs_Sm) {
 
     describe('States transition', function() {
-      var StopGapJs_Sm = requirejs("../sm.js")
+			var stjSm, curState, eventListenerId
+			var config = {
+				"states" : [
+					{ "id" : "root_state" },
+					{ "id" : "second_state" },
+					{ "id" : "third_state" },
+					{ "id" : "last_state" }
+				],
+				"rootStateId" : "root_state",
+				"transitions" : [
+					{
+						srcStateId : "root_state",
+						dstStateId : "second_state",
+						type : "simple"
+					},
+					{
+						srcStateId : "second_state",
+						dstStateId : "third_state",
+						type : "simple"
+					},
+					{
+						srcStateId : "third_state",
+						dstStateId : "last_state",
+						type : "simple"
+					},
+					{
+						srcStateId : "last_state",
+						dstStateId : "root_state",
+						type : "final_rewind"
+					},
+					{
+						srcStateId : "third_state",
+						dstStateId : "root_state",
+						type : "mid_rewind"
+					}
+				]
+			}
+			stjSm = new StopGapJs_Sm().init(config)
 
-      var config = {
-        "states" : [
-          { "id" : "root_state" },
-          { "id" : "second_state" },
-          { "id" : "third_state" },
-          { "id" : "last_state" }
-        ],
-        "rootStateId" : "root_state",
-        "transitions" : [
-          {
-            srcStateId : "root_state",
-            dstStateId : "second_state",
-            type : "simple"
-          },
-          {
-            srcStateId : "second_state",
-            dstStateId : "third_state",
-            type : "simple"
-          },
-          {
-            srcStateId : "third_state",
-            dstStateId : "last_state",
-            type : "simple"
-          },
-          {
-            srcStateId : "last_state",
-            dstStateId : "root_state",
-            type : "final_rewind"
-          },
-          {
-            srcStateId : "third_state",
-            dstStateId : "root_state",
-            type : "mid_rewind"
-          }
-        ]
-      }
-      var stjSm = new StopGapJs_Sm().init(config)
-      var curState = stjSm.getCurState()
-      var transitions
+			describe('Root State', function() {
+				curState = stjSm.getCurState()
+	      it ('it must have "root_state" as id',
+	        function() {
+	          assert(curState.id === "root_state")
+	        }
+	      )
+	      it ('.getTransitiona must give only one transition which is of type "simple"',
+	        function() {
+	          var transitions = curState.getTransitions()
 
-      it ('Root State (first one) must have "root_state" as id',
-        function() {
-          console.log(curState)
-          assert(curState.id === "root_state")
-        }
-      )
+	          assert(typeof curState.getTransitions === "function")
+	          assert(transitions['simple'])
+	          assert(! transitions['mid_rewind'])
+	          assert(! transitions['final_rewind'])
+	        }
+	      )
+			})
 
-      it ('.getTransitiona on rootState must give only one transition which is of type "simple"',
-        function() {
-          transitions = curState.getTransitions()
+			describe('After First transition', function() {
+				var transitions
 
-          assert(typeof curState.getTransitions === "function")
-          assert(transitions.simple)
-          assert(! transitions.mid_rewind)
-          assert(! transitions.final_rewind)
-        }
-      )
+				before(function() {
+					transitions = curState.getTransitions()
+				})
 
-      it ('After first transition, event must been thrown with a simple transition as event detail',
-        function(ok) {
-          stjSm.addEventListener('transit',
-            function(event) {
-              assert(event)
-              assert(event.type === 'transit')
-              assert(event.detail.transition)
-              assert(event.detail.transition.type === "simple")
-              ok()
-            }
-          )
-          curState = transitions.simple.execute()
-        }
-      )
+				it ('Event must been thrown with a simple transition as event detail',
+	        function(ok) {
 
-      it ('After first transition, current state must be "second_state"',
-        function() {
-          assert(typeof curState.getTransitions === "function")
-          assert(transitions.simple)
-          assert(! transitions.mid_rewind)
-          assert(! transitions.final_rewind)
-        }
-      )
+						eventListenerId = stjSm.addEventListener('transit',
+	            function(event) {
+								stjSm.removeEventListener(eventListenerId)
+	              assert(event)
+	              assert(event.type === 'transit')
+	              assert(event.detail.transition)
+	              assert(event.detail.transition.type === "simple")
+	              ok()
+	            }
+	          )
+						curState = transitions.simple.execute()
+						transitions = curState.getTransitions()
+	        }
+	      )
+
+	      it ('Current state must be "second_state" whith only one next transitin of type simple',
+	        function() {
+	          assert(typeof curState.getTransitions === "function")
+						assert(curState.id === "second_state");
+	          assert(transitions['simple'])
+	          assert(! transitions['mid_rewind'])
+	          assert(! transitions['final_rewind'])
+	        }
+	      )
+			});
+
+			describe('After second transition', function() {
+				it ('Current state must be "third_state" whith two availables transitions',
+	        function() {
+						var transitions = curState.getTransitions()
+						curState = transitions['simple'].execute()
+						transitions = curState.getTransitions()
+
+						assert(curState.id === "third_state");
+						assert(typeof curState.getTransitions === "function")
+	          assert(transitions['simple'])
+	          assert(transitions['mid_rewind'])
+	          assert(! transitions.final_rewind)
+	        }
+	      )
+			})
     })
   })
